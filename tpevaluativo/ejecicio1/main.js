73febb4ca1fe5218eca5b4c5407cd25972b18a70
luci0310 <lucianamartinez898@gmail.com>
2022-05-12 15:53:01 -0300

    tp evaluativo

--- tpevaluativo/ejecicio1/main.js
+++ tpevaluativo/ejecicio1/main.js
@@ -2,18 +2,19 @@ function calcular(){
     const resultado=document.getElementById("auto").value
     let respuesta= null
     if(resultado==1750000){
-    respuesta=(1750000-((15/100)*1750000))
-    //fort ka
+        respuesta=(1750000-((15/100)*1750000))
+        //fort ka
     }
     else if(resultado==1950000){
-    respuesta=(1950000-((5/100)*1950000))
-    //ford fiesta
+        respuesta=(1950000-((5/100)*1950000))
+        //ford fiesta
     }
     else if(resultado==2560000){
-    respuesta=(2560000-((10/100)*2560000))
-    //ford focus
+        respuesta=(2560000-((10/100)*2560000))
+        //ford focus
     }
-document.getElementById("d_resultado").textContent=respuesta
+    
+    document.getElementById("d_resultado").textContent=respuesta
 }
 const boton=document.getElementById("btn_calcular")
 boton.addEventListener("click",calcular)
--- tpevaluativo/ejercicio2/main.js
+++ tpevaluativo/ejercicio2/main.js
@@ -1,24 +1,23 @@
 const calcular=(d1)=>{
-if(d1>=14 && d1<=32){
-    const t1=("temperatura baja")
-    return t1
-}else if(d1>32 && d1<=68){
-    const t3=("temperatura alta")
-    return t3
-}else if(d1>68 && d1<=96){
-    const t4=("temperatura alta")
-    return t4
-}else{
-    const t4=("temperatura desconocida")
-    return t4
-}
+    if(d1>=14 && d1<=32){
+        return 'temperatura baja'
+    }else if(d1>32 && d1<=68){
+        const t3=("temperatura adecuada")
+        return t3
+    }else if(d1>68 && d1<=96){
+        const t4=("temperatura alta")
+        return t4
+    }else{
+        const t4=("temperatura desconocida")
+        return t4
+    }
 }
 
 const mostrar=()=>{
-const dato1=document.getElementById("dato1").value
-//ejecutamos la funcion
-const respuesta=calcular(dato1)
-document.getElementById("h_resultado").textContent=respuesta
+    const dato1=document.getElementById("dato1").value
+    //ejecutamos la funcion
+    const respuesta=calcular(dato1)
+    document.getElementById("h_resultado").textContent=respuesta
 }
 
 //encontrar/obtener el boton y guardarlo en la constante
