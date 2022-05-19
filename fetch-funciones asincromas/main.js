c1cd99121eaf8c87545b28e2091f4795f40644ad
luci0310 <lucianamartinez898@gmail.com>
2022-05-19 17:04:21 -0300

    terminando

--- fetch-funciones asincromas/main.js
+++ fetch-funciones asincromas/main.js
@@ -3,12 +3,13 @@ async function obtener_datos()
 {
     //realiza una solicitud htpp a traves del metodo GET
     //es una comunicacion con la API
+    //emploit o ruta
     //el await es para asegurarse que si no llega toda la informacion de una linea, no pase a la siguiente 
     const datos=await fetch('https://jsonplaceholder.typicode.com/posts')
     //indicando que los datos son un json
     const datos_definitivos=await datos.json()
-    //buscando  los datos
+    //recorrer el array de objetos
     datos_definitivos.forEach(element => {
-        console.log(element,title)
+        console.log(element.title)
     });
 }
