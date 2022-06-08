7379a36a0a729ffb6d4bb0e9cd2a5a3a657762f0
luci0310 <lucianamartinez898@gmail.com>
2022-06-08 17:05:40 -0300

    ejercicio 2

--- poo_ej2/batman.js
+++ poo_ej2/batman.js
@@ -5,14 +5,25 @@ export default class Batman{
     async consumir_api(){
         const datos= await fetch('https://fake-movie-database-api.herokuapp.com/api?s=batman')
         const variable=await datos.json()
-
+        let cajitas=[]
         for (const element of variable.Search) {
-            console.log( `
-            Title:${element.Title}
-            Year:${element.Year}
-            Poster:${element.Poster}
-            `)
+            let cajita=`
+            <div class="col-lg-3">
+            <div class="card" style="width: 18rem;">
+                <img src=${element.Poster} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Titulo:${element.Title}</h5>
+                    <p class="card-text">Año de lanzamiento:${element.Year}</p>
+                </div>
+            </div>
+            </div>`
+            cajitas.push(cajita)
         }
-        
+        document.getElementById("row_datos").innerHTML=cajitas.join('')
+
+
     }
+    
 }
+
+
--- poo_ej2/main.js
+++ poo_ej2/main.js
@@ -1,7 +1,10 @@
 import Batman from "./batman.js"
+
 function ejecutar(){
     const batman=new Batman()
     batman.consumir_api()
 }
-document.getElementById("btn").addEventListener("click",ejecutar)
+
+
+ejecutar()
 
