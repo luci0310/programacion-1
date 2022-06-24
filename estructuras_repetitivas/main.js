11f1616383661535508ca5ac6b84a637640db096
luci0310 <lucianamartinez898@gmail.com>
2022-06-24 17:10:27 -0300

    estructuras rpetitivas

---
+++ estructuras_repetitivas/main.js
@@ -0,0 +1,19 @@
+let numeros=[1,2,3,4,5,6,7,8,9,10]
+//for of 
+function mostrar1(){
+    for (const numeros of object) {
+        
+    }
+}
+
+//for in
+
+//for each
+function mostrar2(){
+    numeros.forEach(element => {
+        if(numeros/2==0){
+            const guardo=numeros
+            console.log(guardo)
+        }
+    });
+}
