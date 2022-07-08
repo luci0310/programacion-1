18f65dad75027911147f562e5a9985ee4c7e8c1d
luci0310 <lucianamartinez898@gmail.com>
2022-07-08 17:00:50 -0300

    cambios

--- poo_3/producto.js
+++ poo_3/producto.js
@@ -32,6 +32,7 @@ export default class Producto{
             localStorage.setItem("Productos", JSON.stringify(lista_productos))
         }
         this.obtener_productos()
+        this.vaciar_formulario()
     }
     
     obtener_productos(){
@@ -92,6 +93,12 @@ export default class Producto{
 
         document.getElementById("btn_guardar").style.display="block"
         document.getElementById("btn_actualizar").style.display="none"
+
+        this.vaciar_formulario()
     }
+    vaciar_formulario(){
+        document.getElementById("form_producto").reset()
+    }
+    
 }
 
